cb808c12b997ce825544207fd0976399a0815bd1
Frankie Fan <33444337+superfrankiefan@users.noreply.github.com>
2023-05-09 15:14:10 +0800

    Update app.py
    change test001 to test

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def get_version():
 
 @app.route('/test')
 def get_test():
-    return '<h1>You are accessing /test001 endpoint</h1>'
+    return '<h1>You are accessing /test endpoint</h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
